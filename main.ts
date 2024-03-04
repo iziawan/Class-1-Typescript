33ba853c485f4613ea02559a75c325a1b2a37646
iziawan <162214280+iziawan@users.noreply.github.com>
2024-03-05 02:42:45 +0500

    ok

---
+++ main.ts
@@ -0,0 +1,3 @@
+console.log('Hello World');
+console.log("Hello, World!");
+console.log('Thankyou!Kamran Khan Tessori,Sir Zia,Sir Daniyal,Sir Ameen Aalam,Sir Zeeshan Hanif,Miss Hira Khan,Sir Qasim,Sir Isfihan Ahmed,Miss Sadia');
